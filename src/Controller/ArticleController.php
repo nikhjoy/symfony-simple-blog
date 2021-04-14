383a1ee7d777225eeeb2aa5b6282b98f0421cd75
Nikhil Joy <nikh.liberty77@gmail.com>
2021-04-15 00:52:47 +0530

    design and listing done

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,15 +4,20 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ArticleController
+class ArticleController extends Controller
 {
     /**
      * @Route("/")
      * @Method({"GET"})
      */
     public function index() {
-        return new Response('<html><body>hello</body></html>');
+        //return new Response('<html><body>hello</body></html>');
+        $articles = ['Article 1', 'Article 2'];
+
+        return $this->render('articles/index.html.twig', array
+    ('articles' => $articles));
     }
 }
 
