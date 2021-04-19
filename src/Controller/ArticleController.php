0a8863e060018f298e2b1ce3d7c0884d1a9da095
Nikhil Joy <nikh.liberty77@gmail.com>
2021-04-20 02:13:13 +0530

    form post request done

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -49,11 +49,22 @@ class ArticleController extends Controller
          ))
          ->getForm();
 
-         return $this->render('articles/new.html.twig', array(
-             'form' => $form->createView()
-         ));
-    }
+        $form->handleRequest($request);
 
+        if($form->isSubmitted() && $form->isValid()) {
+            $article = $form->getData();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_list');
+        }
+
+        return $this->render('articles/new.html.twig', array(
+            'form' => $form->createView()
+        ));
+
+        }
     /**
      * @Route("/article/{id}", name="article_show")
      */
